655b3339d0a90855ebc8b834e4ca6b9f593b058e
VaeVictis <VaeVictis@vaevictiss-mbp.home>
2017-02-26 21:54:40 +0100

    recursive findFile not working

--- source/main.py
+++ source/main.py
@@ -9,6 +9,9 @@ class Node:
         self.id = id
         self.value = value
         self.components = []
+        self.parent = None
+        self.dependencies = []
+        self.basePath = ""
         self.componentsBasePath = ""
 
     def __str__(self):
@@ -23,33 +26,64 @@ class VueNode(Node):
         importString = ""
         componentsString = ""
         componentEl = ""
-        for component in self.components:
+        for component in self.dependencies:
             importPath = os.path.normpath(self.componentsBasePath + "/" + component.value)
-            importString += "import %s from '.%s/%s.vue'\n" % (
-                component, importPath, component.value)
+            print(self)
+            print(findFileFromNode(self,"",component,False))
+            # print(component.basePath)
+            importString += "import %s from '%s'\n" % (
+                component, findFile(component.value))
             componentsString += "%s," % (component)
             # print(componentsString)
         template = "<template><div id='%s'>%s</div></template>\n<script> %s export default { name: '%s', components: { %s }, } </script>\n<style></style>" % (
             self.value, componentEl, importString, self.value, componentsString)
         return template
 
-    def create(self, basePath):
-        if not os.path.exists(basePath + '/' + self.componentsBasePath):
-            os.makedirs(basePath + '/' + self.componentsBasePath)
+    def create(self):
+        if not os.path.exists(self.basePath + '/' + self.componentsBasePath):
+            os.makedirs(self.basePath + '/' + self.componentsBasePath)
 
         template = self.render()
 
-        if not os.path.exists(basePath):
-            os.makedirs(basePath)
+        if not os.path.exists(self.basePath):
+            os.makedirs(self.basePath)
 
-        open("{}.vue".format(basePath + '/' + self.value), 'w').write(template)
+        open("{}.vue".format(self.basePath + '/' + self.value), 'w').write(template)
 
-        print("Created %s" %(self.value + '.vue'))
+        print("Created %s" % (self.value + '.vue'))
 
         for component in self.components:
-            newPath = os.path.normpath(basePath + '/' + self.componentsBasePath + '/' + component.value)
-            component.create(newPath)
+            component.create()
 
+
+def findFileFromNode(start,path,toFind,wentDown):
+    if not start:
+        return None
+    if start.id == toFind.id:
+        print(path)
+        # return path
+    if not searchInChildren(start,path,toFind):
+        return findFileFromNode(start.parent, "../" + path, toFind, False)
+
+def searchInChildren(start,path,toFind):
+    for component in start.components:
+          findFileFromNode(component, path + '/' + component.value, toFind,True)
+
+def findFile(name):
+    for r, d, f in os.walk("./"):
+        for files in f:
+            if files == name + '.vue':
+                return os.path.join(r, files)
+
+def createAllPathes(root,basePath):
+    root.basePath = basePath
+    for component in root.components:
+            newPath = os.path.join(basePath, root.componentsBasePath, component.value)
+            createAllPathes(component,newPath)
+
+def createAll(root,destinationPath):
+    createAllPathes(root,destinationPath)
+    root.create()
 
 def usage():
     print("usage: python3 main.py [xmlFile] [destinationPath]")
@@ -61,39 +95,48 @@ def parse(xmlFileName):
     with open(xmlFileName) as fd:
         doc = xmltodict.parse(fd.read())
         for cell in (doc['mxGraphModel']['root']['mxCell']):
-
             if not cell.get('@edge'):
                 # node
                 if cell.get('@value'):
                     id = cell.get("@id")
                     # remove html junk in the title
-                    value = BeautifulSoup(cell.get("@value")).text
+                    value = BeautifulSoup(cell.get("@value"), "lxml").text
                     node = VueNode(id, value)
                     cache[node.id] = node
                     graph.append(node)
+
         for cell in (doc['mxGraphModel']['root']['mxCell']):
             if cell.get('@edge'):
                 source = cache[cell.get('@source')]
-                target = cache[cell.get("@target")]
-                if source not in target.components:
-                    target.components.append(source)
+                target = cache[cell.get('@target')]
+                if (cell.get('@value') == "Use"):
+                    if target not in source.dependencies:
+                        source.dependencies.append(target)
+                else:
+                    if source not in target.components:
+                        target.components.append(source)
+                        source.parent = target
+
         del cache
         return graph
 
 
 def main():
-    xmlFileName = sys.argv[1]
-    destinationPath = sys.argv[2]
+    # xmlFileName = sys.argv[1]
+    # destinationPath = sys.argv[2]
 
-    try:
-        graph = parse(xmlFileName)
-        graph[0].componentsBasePath = "/components"
-        graph[0].create(destinationPath)
-        print('done.')
+    xmlFileName = "d.xml"
+    destinationPath = "./"
+    # try:
+    graph = parse(xmlFileName)
+    graph[0].componentsBasePath = "components"
+    createAll(graph[0],destinationPath)
+    # graph[0].create(destinationPath)
+    print('done.')
 
-    except Exception as e:
-        print(e)
-        usage()
+    # except Exception as e:
+    #     print(e)
+    #     usage()
 
 
 main()
